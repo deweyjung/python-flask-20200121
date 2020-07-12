d2fd7c2e8b6cafa67bb819a05cd7e4be5061934e
dongwoojung <dongwoo.jung@kbfg.com>
2020-07-12 14:54:05 +0900

    .

--- src/package/myapp/util/util.py
+++ src/package/myapp/util/util.py
@@ -3,6 +3,45 @@
 # for the resource http://localhost:8080/file
 import http.client
 
+# for https conn = http.client.HTTPSConnection("www.python.org") and normally... module urllib.request
+# # HTTP Error
+#
+# import urllib.request
+# import urllib.parse
+#
+# # trying to read the URL
+# try:
+#     x = urllib.request.urlopen('https://www.google.com / search?q = test')
+#     print(x.read())
+#
+# # Catching the exception generated
+# except Exception as e :
+#     print(str(e))
+# # importing robot parser class
+# import urllib.robotparser as rb
+#
+# bot = rb.RobotFileParser()
+#
+# # checks where the website's robot.txt file reside
+# x = bot.set_url('https://www.geeksforgeeks.org / robot.txt')
+# print(x)
+#
+# # reads the files
+# y = bot.read()
+# print(y)
+#
+# # we can crawl the main site
+# z = bot.can_fetch('*', 'https://www.geeksforgeeks.org/')
+# print(z)
+#
+# # but can not crawl the disallowed url
+# w = bot.can_fetch('*', 'https://www.geeksforgeeks.org / wp-admin/')
+# print(w)
+
+
+
+
+# case1
 BODY = ""
 conn = http.client.HTTPConnection("ec2-18-188-30-68.us-east-2.compute.amazonaws.com", 8080)
 conn.request("GET", "/")
@@ -10,7 +49,7 @@ response = conn.getresponse()
 print(response.status, response.reason)
 print(response.read())
 
-
+# case2
 # import http.client, urllib.parse
 # params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
 # headers = {"Content-type": "application/x-www-form-urlencoded",
